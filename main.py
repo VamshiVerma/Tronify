55e458eead7097ae76bc086b21f557d490132c06
Vamshi Verma <vamshipaili@outlook.com>
2022-11-12 13:34:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -251,7 +251,7 @@ if selected == "Home":
             sam.empty()
             st.subheader("Tron Transaction Details")
 
-            st.json(k,expanded=False)
+            st.json(k,collapse=True)
             st.success('**Tron HashID: **'+str(k['id']))
             
 
