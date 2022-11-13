28dc3213ac9cc0006cc8859ab410eab0dab96b49
Vamshi Verma <vamshipaili@outlook.com>
2022-11-13 08:14:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ if selected == "Home":
                 df = pd.read_excel(uploaded_file, engine='openpyxl')
             st.header(df.iloc[0,0])
 
-            if(!(df.iloc[0,0] == null || df.iloc[0,0].isEmpty() || df.iloc[0,0].trim().isEmpty())):
+            if(df.empty == False):
                 st.header(df.size)
                 contracts= df['Contract'].tolist()
                 names = df['Name'].tolist()
