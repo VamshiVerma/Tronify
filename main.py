c79e4ff17903ba7d9def62841dff1e70eb45da7d
Vamshi Verma <vamshipaili@outlook.com>
2022-11-13 09:05:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -254,7 +254,7 @@ if selected == "Input":
             
                 d=  st.dataframe(pd.DataFrame(table_values))
                 test = 'https://gateway.ipfs.io/ipfs/{}'
-                st.text(table_values['ipfs_cid'])
+                st.text(table_values[0]['ipfs_cid'])
             # df.loc[0, 'ipfs_cid']
                 
                 #st.title(d.iat[0, 1])
